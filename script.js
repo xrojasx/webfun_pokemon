8c2022475623ddd37da49cc1fbe9e2b5bf7d9bd0
Ricardo Rojas <xrojas@att.net>
2017-12-12 18:34:57 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    for (var i =1; i<151; i++){
+    $('#Pokemon').append(`<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png">`)
+    }
+    
+});
